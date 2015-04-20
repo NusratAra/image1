2f3296501d50132fe94d4bfcb335ab919fddf203
Nusrat Mubin Ara <mubin.ara.24@gmail.com>
2015-04-20 23:45:30 +0600

    Adding image smothing

--- src/image1/Image1.java
+++ src/image1/Image1.java
@@ -8,13 +8,16 @@ import static com.googlecode.javacv.cpp.opencv_highgui.*;
 public class Image1 {
 
     public static void main(String[] args) {
-        
+
         //BGR to HSV
+        //Image Smoothing / Filtering / Blur
         IplImage img = cvLoadImage("r.jpg");
-        IplImage gray = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 3);
-        cvCvtColor(img, gray, CV_BGR2HSV);
+//      IplImage gray=IplImage.create(img.width(),img.height(), IPL_DEPTH_8U, 3);
+//      cvCvtColor(img, gray, CV_BGR2HSV);
+        IplImage dst = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, img.nChannels());
+        cvSmooth(img, dst, CV_MEDIAN, 5);
         CanvasFrame cv = new CanvasFrame("My Frame");
         cv.setDefaultCloseOperation(CanvasFrame.EXIT_ON_CLOSE);
-        cv.showImage(gray);
+        cv.showImage(dst);
     }
 }
