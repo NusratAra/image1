20a02dbe85458cc93c23d3ae2720353c9c95646b
Nusrat Mubin Ara <mubin.ara.24@gmail.com>
2015-04-21 22:31:15 +0600

    Adding image cropping

--- src/image1/Image1.java
+++ src/image1/Image1.java
@@ -11,13 +11,35 @@ public class Image1 {
 
         //BGR to HSV
         //Image Smoothing / Filtering / Blur
-        IplImage img = cvLoadImage("r.jpg");
-//      IplImage gray=IplImage.create(img.width(),img.height(), IPL_DEPTH_8U, 3);
-//      cvCvtColor(img, gray, CV_BGR2HSV);
-        IplImage dst = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, img.nChannels());
-        cvSmooth(img, dst, CV_MEDIAN, 5);
-        CanvasFrame cv = new CanvasFrame("My Frame");
-        cv.setDefaultCloseOperation(CanvasFrame.EXIT_ON_CLOSE);
-        cv.showImage(dst);
+//        IplImage img = cvLoadImage("r.jpg");
+////      IplImage gray=IplImage.create(img.width(),img.height(), IPL_DEPTH_8U, 3);
+////      cvCvtColor(img, gray, CV_BGR2HSV);
+//        IplImage dst = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, img.nChannels());
+//        cvSmooth(img, dst, CV_MEDIAN, 5);
+//        CanvasFrame cv = new CanvasFrame("My Frame");
+//        cv.setDefaultCloseOperation(CanvasFrame.EXIT_ON_CLOSE);
+//        cv.showImage(dst);
+
+
+        //Image Crop
+        //prblm**********
+        IplImage src = cvLoadImage("r.jpg");
+        int upperLeftX = 100;
+        int upperLeftY = 100;
+        int downRightX = 200;
+        int downRightY = 200;
+        int width = downRightX - upperLeftX;
+        int height = downRightY - upperLeftY;
+        CvRect r = new CvRect(width, height, upperLeftX, upperLeftY);
+        cvSetImageROI(src, r); //set region of interest
+        //cvSetImageROI is before IplImage dst
+        IplImage dst = cvCreateImage(cvGetSize(src), src.depth(), src.nChannels());
+        cvCopy(src, dst);//Copy original image(only ROI) to dst
+        cvResetImageROI(src); //reset ROI
+        CanvasFrame canvas = new CanvasFrame("MyImage");
+        canvas.setDefaultCloseOperation(CanvasFrame.EXIT_ON_CLOSE);
+        canvas.showImage(dst);
+
+
     }
 }
